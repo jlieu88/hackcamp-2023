5d05f3b3a148d15f018e50b5409cc2932354b3eb
jlieu88 <j_lieu@outlook.com>
2023-11-19 13:43:23 -0800

    added save button for color

--- popup/popup.js
+++ popup/popup.js
@@ -15,4 +15,11 @@ async function onOpen() {
     });
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("saveColor").addEventListener("click", function() {
+        var input = document.getElementById("SimpleColor").value;
+        document.getElementById("demo").innerHTML = "The color " + input + " has been saved.";
+    });
+})
+
 onOpen();
